5182231408cd57aea04f337eb253bdb8ccdd521e
Joseph Dugan <Joseph.Michael.Dugan@gmail.com>
2022-10-07 16:13:30 -0600

    updated fetch on login.jsx

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -23,7 +23,7 @@ export default function Login() {
     const verifyPassword = { ...form };
 
     await fetch("http://localhost:5001/login", {
-      method: "PUT",
+      metho: "PUT",
       headers: {
         "Content-Type": "application/json",
       },
