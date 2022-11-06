dd12a5bf2b03e24518ef0baf09b6ea7b3ec71da6
Joseph Dugan <Joseph.Michael.Dugan@gmail.com>
2022-11-06 12:19:32 -0700

    updated

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,4 @@
 // const bcrypt = require("bcrypdt");
-// const saltRounds = 103;
+// const saltRounds = 10;
 // const myPlaintextPassword = "s0//P4$$w0rD";
 // const someOtherPlaintextPassword = "not_bacon";
